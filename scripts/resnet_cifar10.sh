0178b26510d31f66b96550f0dd8ae1c9fb148985
Shiyu Li <timlee0212@outlook.com>
2022-03-15 23:50:07 -0400

    Clean up files.

--- scripts/resnet_cifar10.sh
+++
@@ -1,6 +0,0 @@
-#python3.9 cifar10.py -w 32 -a 32 -gtar 1.0 -spbp -pact -gpu 0 -s -lr 0.001 -th 1.0 -ft #91.2% +sp:0
-#python3.9 cifar10.py -w 8 -a 8 -gtar 1.0 -spbp -pact -gpu 0 -s -lr 0.001 -th 1.0 -ft -model w32a32 #91.2% +sp:0
-#python3.9 cifar10.py -w 4 -a 4 -gtar 1.0 -spbp -pact -gpu 0 -s -lr 0.1 -th 1.0 -ft -model w32a32 #91.2% +sp:0
-python3.9 cifar10.py --wbits 32 --abits 32 --gpu 3 --save --arch resnet20 --batch_size 128 --dense --lr 1e-1 --sched step --warmup_epochs 0 --decay_epochs 50 --opt SGD --mixup 0
-
-
--- test.py
+++
@@ -1,6 +0,0 @@
-from core import Config, Experiment
-
-conf = Config("configs/example.yaml")
-exp = Experiment(conf)
-
-exp.run()

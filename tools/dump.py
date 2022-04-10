35c0aa93819aac27d9870dee8964a3e065a0a216
qilinzheng <qz91@duke.edu>
2022-04-10 19:21:08 -0400

    added visualization sparse attention map

--- tools/dump.py
+++ tools/dump.py
@@ -36,6 +36,13 @@ class Dumper(Inspector):
         self.trainer.evalModel(self.model, n_iter=1)
         np.save(os.path.join(self.path, self.config.Model.model_name +
                 "_mask.npy"), self.layer_masks)
+        import matplotlib.pyplot as plt
+        for mask in self.layer_masks.values():
+            for head in range(mask.shape[1]):
+                image = mask[0,head,:,:]
+                plt.imshow(image)
+                plt.show()
+  
 
 
 parser = argparse.ArgumentParser(description='Training Config', add_help=True)
